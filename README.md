966f785faf5b5513fdafc48307d78bf4a11ecd64
yangyi <yyanglike@sina.com>
2024-08-01 16:53:26 +0800

    完整的单进程的函数调用跟踪。

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-sudo bpftrace trace_vfs_write.bt
 
-
+## 跟踪指定函数
 sudo bpftrace -e 'kprobe:vfs_write { printf("vfs_write called\n"); }'
 
 
+## 指定进程id进行函数调用跟踪
 sudo bpftrace -e '
 tracepoint:syscalls:sys_enter_write
 {
@@ -14,15 +14,28 @@ tracepoint:syscalls:sys_enter_write
 
 
 
+## 查询可以追踪的函数的命令：
+ sudo ls /sys/kernel/debug/tracing/events/syscalls/
+[sudo] password for yangyi: 
+enable                             sys_enter_listxattr               sys_enter_set_mempolicy_home_node  sys_exit_fspick                   sys_exit_prlimit64
+filter                             sys_enter_llistxattr              sys_enter_setns                    sys_exit_fstatfs                  sys_exit_process_madvise
+sys_enter_accept                   sys_enter_lremovexattr            sys_enter_setpgid                  sys_exit_fsync                    sys_exit_process_mrelease
+sys_enter_accept4                  sys_enter_lseek                   sys_enter_setpriority              sys_exit_ftruncate                sys_exit_process_vm_readv
+sys_enter_access                   sys_enter_lsetxattr               sys_enter_setregid                 sys_exit_futex                    sys_exit_process_vm_writev
+sys_enter_acct                     sys_enter_madvise                 sys_enter_setresgid                sys_exit_futex_waitv              sys_exit_pselect6
+sys_enter_add_key                  sys_enter_mbind                   sys_enter_setresuid                sys_exit_futimesat                sys_exit_ptrace
+sys_enter_adjtimex                 sys_enter_membarrier              sys_enter_setreuid                 sys_exit_getcpu                   sys_exit_pwrite64
+sys_enter_alarm                    sys_enter_memfd_create            sys_enter_setrlimit                sys_exit_getcwd                   sys_exit_pwritev
+sys_enter_arch_prctl               sys_enter_memfd_secret            sys_enter_set_robust_list          sys_exit_getdents                 sys_exit_pwritev2
+sys_enter_bind                     sys_enter_migrate_pages           sys_enter_setsid                   sys_exit_getdents64               sys_exit_quotactl
+sys_enter_bpf                      sys_enter_mincore                 sys_enter_setsockopt               sys_exit_getegid                  sys_exit_quotactl_fd
 
-测试
-tracepoint:syscalls:sys_enter_writev
+## 查询需要跟踪的函数的调用参数：
+sudo cat /sys/kernel/debug/tracing/events/syscalls/sys_enter_pwritev2/format
 
 
-
+## 跟踪工具相关安装：
 sudo apt install linux-tools-$(uname -r) linux-cloud-tools-$(uname -r)
-
-sudo cat /sys/kernel/debug/tracing/events/syscalls/sys_enter_pwrite64/format
 
 
 uname -r
@@ -31,18 +44,4 @@ or 6.2
 sudo apt-get install linux-hwe-6.2-tools-common
 
 
-sudo nano /usr/src/linux/.config
-
-
-sudo bpftrace -e '
-tracepoint:syscalls:sys_enter_pwrite64
-{
-    if (pid == $TARGET_PID) {
-        printf("write pid: %d, fd: %d, buf: %p, size: %lu, offset: %lu\n", 
-               pid, 
-               args->fd, 
-               args->buf, 
-               args->count, 
-               args->pos);
-    }
-}'
+## 查询需要看的例子参考check.sh
--- check.sh
+++ check.sh
@@ -8,18 +8,21 @@ fi
 
 TARGET_PID=$1
 
-# 使用 bpftrace 捕获 sys_enter_write、sys_enter_writev 和 sys_enter_pwrite64 事件
-sudo bpftrace -e "
+# 使用 bpftrace 捕获 sys_enter_write、sys_enter_writev、sys_enter_pwrite64、sys_enter_pwritev 和 sys_enter_pwritev2 事件
+sudo bpftrace -e '
+#include <linux/rwsem.h>
+#define rwf_t unsigned int
+
 tracepoint:syscalls:sys_enter_write
 {
-    if (pid == $TARGET_PID) {
-        printf(\"write pid: %d, fd: %d, size: %d\n\", pid, args->fd, args->count);
+    if (pid == '$TARGET_PID') {
+        printf("write pid: %d, fd: %d, size: %d\n", pid, args->fd, args->count);
     }
 }
 tracepoint:syscalls:sys_enter_pwrite64
 {
-    if (pid == $TARGET_PID) {
-        printf(\"write pid: %d, fd: %d, buf: %p, size: %lu, offset: %lu\n\", 
+    if (pid == '$TARGET_PID') {
+        printf("pwrite64 pid: %d, fd: %d, buf: %p, size: %lu, offset: %lu\n", 
                pid, 
                args->fd, 
                args->buf, 
@@ -29,14 +32,37 @@ tracepoint:syscalls:sys_enter_pwrite64
 }
 tracepoint:syscalls:sys_enter_writev
 {
-    if (pid == $TARGET_PID) {
-        printf(\"writev pid: %d, fd: %d, vec: %p, vlen: %lu\n\", 
+    if (pid == '$TARGET_PID') {
+        printf("writev pid: %d, fd: %d, vec: %p, vlen: %lu\n", 
                pid, 
                args->fd, 
                args->vec, 
                args->vlen);
     }
-}" | while IFS= read -r line; do
+}
+tracepoint:syscalls:sys_enter_pwritev
+{
+    if (pid == '$TARGET_PID') {
+        printf("pwritev pid: %d, fd: %d, vec: %p, vlen: %lu\n", 
+               pid, 
+               args->fd, 
+               args->vec, 
+               args->vlen);
+    }
+}
+tracepoint:syscalls:sys_enter_pwritev2
+{
+    if (pid == '$TARGET_PID') {
+        printf("pwritev2 pid: %d, fd: %lu, vec: %p, vlen: %lu, pos_l: %lu, pos_h: %lu, flags: %u\n", 
+               pid, 
+               args->fd, 
+               args->vec, 
+               args->vlen, 
+               args->pos_l, 
+               args->pos_h, 
+               args->flags);
+    }
+}' | while IFS= read -r line; do
     # 过滤掉启动消息
     if [[ "$line" == *"Attaching"* ]]; then
         continue
@@ -44,23 +70,38 @@ tracepoint:syscalls:sys_enter_writev
 
     # 解析输出的不同格式数据
     case "$line" in
-        *"write"*)
+        *"write pid"*)
             pid=$(echo "$line" | awk '{print $3}' | tr -d ',')
             fd=$(echo "$line" | awk '{print $5}' | tr -d ',')
             size=$(echo "$line" | awk '{print $7}')
             ;;
-        *"writev"*)
+        *"writev pid"*)
             pid=$(echo "$line" | awk '{print $3}' | tr -d ',')
             fd=$(echo "$line" | awk '{print $5}' | tr -d ',')
             vec=$(echo "$line" | awk '{print $7}' | tr -d ',')
             vlen=$(echo "$line" | awk '{print $9}')
             ;;
-        *"pwrite64"*)
+        *"pwrite64 pid"*)
             pid=$(echo "$line" | awk '{print $3}' | tr -d ',')
             fd=$(echo "$line" | awk '{print $5}' | tr -d ',')
             buf=$(echo "$line" | awk '{print $7}' | tr -d ',')
             count=$(echo "$line" | awk '{print $9}')
             offset=$(echo "$line" | awk '{print $11}')
+            ;;
+        *"pwritev pid"*)
+            pid=$(echo "$line" | awk '{print $3}' | tr -d ',')
+            fd=$(echo "$line" | awk '{print $5}' | tr -d ',')
+            vec=$(echo "$line" | awk '{print $7}' | tr -d ',')
+            vlen=$(echo "$line" | awk '{print $9}')
+            ;;
+        *"pwritev2 pid"*)
+            pid=$(echo "$line" | awk '{print $3}' | tr -d ',')
+            fd=$(echo "$line" | awk '{print $5}' | tr -d ',')
+            vec=$(echo "$line" | awk '{print $7}')
+            vlen=$(echo "$line" | awk '{print $9}')
+            pos_l=$(echo "$line" | awk '{print $11}')
+            pos_h=$(echo "$line" | awk '{print $13}')
+            flags=$(echo "$line" | awk '{print $15}')
             ;;
     esac
 
